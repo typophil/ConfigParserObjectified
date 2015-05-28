34c6673fc1d134d76988956ad748f99e8bb09a90
pso <pso@pso.bk.hosteurope.de>
2015-05-28 12:59:26 +0200

    Initial import

---
+++ ConfigParserObjectified.py
@@ -0,0 +1,24 @@
+import ConfigParser
+
+class ConfigObject(object):
+
+    class StandInObject(object):
+        """
+        As object does not have a __dict__ method, you
+        cannot simply add attributes to it
+        """
+        pass
+
+    def __init__(self, cfg_file):
+        PARSER = ConfigParser.ConfigParser()
+        PARSER.read(cfg_file)
+
+        for cfg_section in PARSER.sections():
+            # First round, create objects
+            for cfg_option in PARSER.options(cfg_section):
+                setattr(self, cfg_section, self.StandInObject())
+            # Second round, add attributes
+            for cfg_option in PARSER.options(cfg_section):
+                # Set the values for the section options
+                setattr(getattr(self, cfg_section), cfg_option, PARSER.get(cfg_section, cfg_option))
+
